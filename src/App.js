e670c2b68e474193061ac382d5071ef92039e755
elidvenega <venegaelid@gmail.com>
2022-04-26 20:21:37 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
 
 function App() {
+  const[weight, setWeight] = useState(0);
+  const[height, setHeight] = useState(0);
+  const[bmi, setBmi] = useState('');
+  const[message, setMessage] = useState('');
+
+  let imgSrc = '';
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <h2 className="center">BMI Calculator</h2>
+       <form>
+       <div>
+          <label>Weight (lbs)</label>
+          <input value={weight} />
+        </div>
+        <div>
+          <label>Height (in)</label>
+          <input value={height} />
+        </div>
+        <button className="btn" type='submit'>Submit</button>
+        <button className="btn btn-outline" type='submit'>Submit</button>
+       </form>
+       <div className="center">
+         <h3>Your BMI is: {bmi}</h3>
+         <p>{message}</p>
+       </div>
+       <div className="">
+         <img src={imgSrc} alt=''></img>
+       </div>
+      </div>
     </div>
   );
 }
